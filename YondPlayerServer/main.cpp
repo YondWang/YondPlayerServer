c8dbc111ecec0bbbcde6b83ab38cf541de683b8d
Yond_Wang_homePC <1826297652@qq.com>
2025-07-05 16:43:05 +0800

    1.传输fd，创建子进程并定义入口函数

--- YondPlayerServer/main.cpp
+++ YondPlayerServer/main.cpp
@@ -2,10 +2,160 @@
 #include <unistd.h>
 #include <functional>
 #include <iostream>
+#include <memory.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+class CFunctionBase {
+public:
+    CFunctionBase() = default;
+    virtual ~CFunctionBase() {}
+    virtual int operator()() = 0;
+};
+
+template<typename _FUNCTION_, typename... _ARGS_>
+class CFunction : public CFunctionBase {
+public:
+    CFunction(_FUNCTION_ func, _ARGS_... args) : 
+    m_binder(std::forward<_FUNCTION_>(func), 
+    std::forward<_ARGS_>(args)...)
+    {
+        
+    }
+    virtual ~CFunction() {};
+    virtual int operator()() {
+        return m_binder();
+    }
+    typename std::_Bindres_helper<int, _FUNCTION_, _ARGS_...>::type m_binder;
+};
+
+class CProcess {
+public:
+    CProcess() {
+        m_func = NULL;
+        memset(pipes, 0, sizeof(pipes));
+    }
+    ~CProcess() {
+        if (m_func != NULL) {
+            delete m_func;
+            m_func = NULL;
+        }
+    }
+
+    template<typename _FUNCTION_, typename... _ARGS_>
+    int SetEntryFunction(_FUNCTION_ func, _ARGS_... args) {
+        m_func = new CFunction<_FUNCTION_, _ARGS_...>(func, args...);
+
+        return 0;
+    }
+
+    int CreateSubProcess() {
+        if (m_func == NULL) return -1;
+        int ret = socketpair(AF_LOCAL, SOCK_STREAM, 0, pipes);
+        if (ret == -1) return -2;
+        pid_t pid = fork();
+        if (pid == -1) return -3;
+        if (pid == 0) {
+            // Child process
+            close(pipes[1]);        //close write
+            pipes[1] = 0;
+            return (*m_func)();
+        }
+
+        // Parent process
+        close(pipes[0]);
+        pipes[0] = 0;
+        m_pid = pid;
+        return 0;
+    }
+
+    int SendFD(int fd) {    //主进程完成
+        struct msghdr msg;
+        iovec iov[2];
+
+        iov[0].iov_base = (char*)"YondWang";
+        iov[0].iov_len = 9;
+        iov[1].iov_base = (char*)"edoyun";
+        iov[1].iov_len = 7;
+        msg.msg_iov = iov;
+        msg.msg_iovlen = 2;
+
+        //下面的数据才是我们需要传递的
+        cmsghdr* cmsg = (cmsghdr*)calloc(1, CMSG_LEN(sizeof(int)));
+        if (cmsg == NULL) return -1;
+        cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+        *(int*)CMSG_DATA(cmsg) = fd;
+        msg.msg_control = cmsg;
+        msg.msg_controllen = cmsg->cmsg_len;
+
+        ssize_t ret = sendmsg(pipes[1], &msg, 0);
+        free(cmsg);
+
+        if (ret == -1) {
+            return -2;
+        }
+        return 0;
+    }
+
+    int RecvFD(int& fd) {
+        msghdr msg;
+        iovec iov[2];
+        char buf[][10] = { "", "" };
+        iov[0].iov_base = buf[0];
+        iov[0].iov_len = sizeof(buf[0]);
+        iov[1].iov_base = buf[1];
+        iov[1].iov_len = sizeof(buf[1]);
+        msg.msg_iov = iov;
+        msg.msg_iovlen = 2;
+
+        cmsghdr* cmsg = (cmsghdr*)calloc(1, CMSG_LEN(sizeof(int)));
+        if (cmsg == NULL) {
+            return -1;
+        }
+        cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+        cmsg->cmsg_level = SOL_SOCKET;
+        cmsg->cmsg_type = SCM_RIGHTS;
+        msg.msg_control = cmsg;
+        msg.msg_controllen = CMSG_LEN(sizeof(int));
+        ssize_t ret = recvmsg(pipes[0], &msg, 0);
+        if (ret == -1) {
+            free(cmsg);
+            return -2;
+        }
+        fd = *(int*)CMSG_DATA(cmsg);
+        return 0;
+    }
+
+private:
+    CFunctionBase* m_func;
+    pid_t m_pid;
+    int pipes[2];
+};
+
+int CreateLogServer(CProcess* proc) {
+    return 0;
+}
+
+int CreateClientServer(CProcess* proc) {
+    return 0;
+}
 
 
 int main()
 {
-    printf("%s 向你问好!\n", "YondPlayerServer");
+    CProcess proclog, procclient;
+    proclog.SetEntryFunction(CreateLogServer, &proclog);
+    int ret = proclog.CreateSubProcess();
+    if (ret != 0) {
+        return -1;
+    }
+    procclient.SetEntryFunction(CreateClientServer, &procclient);
+    ret = procclient.CreateSubProcess();
+    if (ret != 0) {
+        return -2;
+    }
+
     return 0;
 }
